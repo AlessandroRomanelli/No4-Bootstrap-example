2e77a6cf09494de57a1d78735a3dda535ae63ab3
Alessandro Romanelli <aleromrod@gmail.com>
2017-06-02 19:51:40 +0200

    Expanded join section, added useful links

--- js/app.js
+++ js/app.js
@@ -99,8 +99,3 @@ function togglePlayback() {
     pauseSong();
   }
 }
-
-function setHalfVolume() {
-    var myAudio = document.getElementById("player");
-    myAudio.volume = 0.1; //Changed this to 0.5 or 50% volume since the function is called Set Half Volume ;)
-}
