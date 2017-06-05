56069f7d26d67e7265581b57ef98d3b0ed280892
Alessandro Romanelli <aleromrod@gmail.com>
2017-06-05 13:21:32 +0200

    Added weapons to arsenal, improved help section

--- js/app.js
+++ js/app.js
@@ -1,75 +1,23 @@
 $(document).ready(() => {
-  $(function () {
+  $(() => {
     $('.weapon-pop').popover({
       container: 'body'
-    })
-  })
-});
-
-$('.popover-dismiss').popover({
-  trigger: 'focus'
-})
-
-// Select all links with hashes
-$('#navbarNav ul li a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      &&
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
-  });
-
-function autoPlayYouTubeModal(){
-  var trigger = $("body").find('[data-toggle="modal"]');
-  trigger.click(function() {
-    var theModal = $(this).data( "target" ),
-    videoSRC = $(this).attr( "data-theVideo" ),
-    videoSRCauto = videoSRC+"?autoplay=1" ;
-    $(theModal+' iframe').attr('src', videoSRCauto);
-    $(theModal+' button.close').click(function () {
-        $(theModal+' iframe').attr('src', videoSRC);
     });
+
+
+    $('#time').one('click', function() {
+      setInterval(() => {
+        let now = new Date();
+        let curTime = formatAMPM(now);
+        $(this).text(curTime);
+      }, 1000);
+    })
   });
-}
 
-$('#videoModal').on('hide.bs.modal', function(e) {
-  var $if = $(e.delegateTarget).find('iframe');
-  var src = $if.attr("src");
-  $if.attr("src", '/empty.html');
-  $if.attr("src", "");
-});
+  $('.popover-dismiss').popover({
+    trigger: 'focus'
+  });
 
-
-$(document).ready(function(){
-  autoPlayYouTubeModal();
 
   $('#operations-cards a').blur(function() {
     setTimeout(() => {
@@ -77,7 +25,57 @@ $(document).ready(function(){
       $(this).next().children().last().collapse('hide');
     }, 300);
   });
+
+  // Select all links with hashes
+  $('#navbarNav ul li a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
+          });
+        }
+      }
+    });
 });
+
+function formatAMPM(date) {
+  var hours = date.getUTCHours()+1;
+  var minutes = date.getUTCMinutes();
+  var seconds = date.getUTCSeconds()
+  var ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  seconds = seconds < 10 ? '0'+seconds : seconds;
+  var strTime = `${hours}:${minutes}:${seconds} ${ampm}`;
+  return strTime;
+}
 
 function pauseSong() {
   var myAudio = document.getElementById("player");
